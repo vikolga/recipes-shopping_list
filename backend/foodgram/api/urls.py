9ceb7aa6e9034f443a4d0d161cdfed8604f8c893
olgavik <ov.reatch@gmail.com>
2023-09-13 19:03:00 +0300

    создаются пользователи, формируются токены, через админку можно подписаться и видно при запросе подписан на пользователя или нет

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -12,5 +12,6 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
