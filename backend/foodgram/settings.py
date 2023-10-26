8feef6c2afd1fdcaab86c90d47239eb37b3d28fa
vikolga <ov.reatch@gmail.com>
2023-10-26 10:24:41 +0300

    внесли исправление в файл настроек

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.2.66', 'foodgramov.hopto.org']
 
 AUTH_USER_MODEL = 'users.CustomUser'
 # Application definition
